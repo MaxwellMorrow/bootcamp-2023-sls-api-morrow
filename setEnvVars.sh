f046d5eda10ec718e48b764925bdfbc85d024763
Maxwell Morrow <mmorrow@dminc.com>
2023-02-09 11:36:53 -0800

    finished my app

--- setEnvVars.sh
+++ setEnvVars.sh
@@ -1,3 +1,3 @@
-export GITHUB_USERNAME="<<username>>"
-export GITHUB_REPO="bootcamp-2022-sls-api-<<lastname>>"
-export BUCKET_NAME="<<username>>-bootcamp-2022-bucket" #username must be all lowercase
+export GITHUB_USERNAME="MaxwellMorrow"
+export GITHUB_REPO="bootcamp-2023-sls-api-morrow"
+export BUCKET_NAME="mmorrow-bootcamp-2022-bucket" #username must be all lowercase
